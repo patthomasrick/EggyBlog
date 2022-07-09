773136c65b2f37b038268e985eded6790d3b17b3
Patrick Thomas <patrick@patrickwthomas.net>
2022-07-09 12:21:35 -0400

    Fix header issues

--- components/header.jsx
+++ components/header.jsx
@@ -6,11 +6,9 @@ export default function Header({ title, description = "" }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
 
-        <title>
-          {title ? title : "Eggy Blog"} | {SHORT_NAME}
-        </title>
+        <title>{`${title ? title : "Eggy Blog"} | ${SHORT_NAME}`}</title>
         <link rel="icon" href="/favicon.ico" />
 
         {description && <meta name="description" content={description} />}
